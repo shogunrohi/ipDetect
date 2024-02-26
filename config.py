54edb376d397bca2bc4ea2d066595d0f5a8cc958
xkingrohi <125758746+xkingrohi@users.noreply.github.com>
2024-02-25 22:05:51 -0500

    Update config.py
    added reset function so results of other IP queries will show

--- config.py
+++ config.py
@@ -2,3 +2,8 @@ def run():
     api_key =  ""
     api_access =  "https://vpnapi.io/api/-?key="
     return api_key, api_access
+
+def reset(api):
+    api_access2 = "https://vpnapi.io/api/~?key="
+    api_key2 = api[api.index("=")+ 1:]
+    return api_access2 + api_key2
