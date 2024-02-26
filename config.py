e1ee9cc8a6e3587c02cfd9b618d6f6e8abbec25f
xkingrohi <125758746+xkingrohi@users.noreply.github.com>
2024-02-26 09:21:59 -0500

    Update config.py
    - added savekey() function to create local file contain api key
    - edited run() function to check if api key already exists in directory

--- config.py
+++ config.py
@@ -1,9 +1,23 @@
+import os.path
 def run():
-    api_key =  ""
-    api_access =  "https://vpnapi.io/api/-?key="
-    return api_key, api_access
+    if os.path.exists("token.txt"):
+        file = open("token.txt", "r")
+        api_key = file.read()
+        file.close()
+        api_access = "https://vpnapi.io/api/~?key="
+        return api_key, api_access
+    else:
+        api_key =  ""
+        api_access = "https://vpnapi.io/api/~?key="
+        return api_key, api_access
 
 def reset(api):
     api_access2 = "https://vpnapi.io/api/~?key="
     api_key2 = api[api.index("=")+ 1:]
     return api_access2 + api_key2
+
+def savekey(tken):
+    file = open("token.txt","w")
+    file.write(tken)
+    file.close()
+
